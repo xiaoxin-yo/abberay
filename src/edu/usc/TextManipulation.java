7e948e9435707ec1dc975009c8d80c9c3cf2cdf9
clifftai <cctai@ucsd.edu>
2014-11-08 22:55:09 -0800

    Smart Definition Attempted
    Text Changed, test cases

--- src/edu/usc/TextManipulation.java
+++ src/edu/usc/TextManipulation.java
@@ -4,8 +4,9 @@ package edu.usc;
 import java.util.ArrayList;
 
 public class TextManipulation {
-	private static String text= "Sulfate (SO42-) is the second-most abundant anion after chloride in the modern ocean. It serves as an easily accessible energy source for sulfate-reducing prokaryotes (SRPs), which are commonly found in organic-rich sediments and play an important role in the decomposition of organic matter. Were these microbes major players in ecosystems during the Archean (before 2.5 billion years ago), when molecular oxygen was virtually absent from both the atmosphere and oceans? Whether this was the case depends on how much sulfate there was in the Archean ocean. Three articles in this issue (1-3) use precise measurements of stable sulfur isotope ratios to investigate how much sulfate there was in the Archean ocean and where that sulfate originated. Scientists use the ratio between two stable sulfur isotopes, 34S and 32S, to trace SRP activity in ancient sedimentary rocks. In sediments, the microbes preferentially reduce 32SO42- compared to 34SO42-, producing iron sulfide minerals that are depleted in 34S (a process called mass-dependent isotopic fractionation). 34S depletion recorded in pyrite (FeS2) minerals is thus a good indicator of SRP activity. Large 34S depletions are seen in the geological records starting about 2.3 billion years ago, but not before (4). One possible interpretation of these data is that there was little SRP activity in the Archean. Another possibility is that these microbes appeared much earlier, but that the Archean sulfate levels were too low for them to selectively reduce 32SO42-; low sulfate levels are known to reduce the degree of isotopic fractionation (5).";
+	private static String text= " hello. alto Testing All Ways (TAW) hello. TAW";
 	private String abbv; 
+	private String longForm;
 	String[] results;
 
 	private ArrayList<Integer> indexes;
@@ -162,8 +163,53 @@ public class TextManipulation {
 			 
 		}
 		for (String s: results)
-			System.out.println(s + "\n");
+			//System.out.println(s + "\n");
+		findLongForm();
+		String test = findBestLongForm ("TAW", longForm);
+		System.out.println(test);
 	}
+	
+	
+	public void findLongForm() {
+		int count = countOccur();
+		
+		for (int i=0; i < count; i++) {
+			int index = indexes.get(i);
+			int start = findSentenceStart(index) + 1;
+			int current = indexes.get(i);
+			longForm = longForm + text.substring(start, current);
+			//System.out.println("i" + i + "x" + x + "y" + y);
+		}
+	} 
+	
+	public String findBestLongForm(String shortForm, String longForm) { 
+		int sIndex; 
+		int lIndex; 
+		char currChar;  
+		sIndex = shortForm.length() - 1; 
+		lIndex = longForm.length() - 1; 
+		for ( ; sIndex >= 0; sIndex--) { 
+		    currChar = Character.toLowerCase(shortForm.charAt(sIndex)); 
+		 if (!Character.isLetterOrDigit(currChar)) 
+		     continue; 
+		
+		 while ( 
+		((lIndex >= 0) && 
+		(Character.toLowerCase(longForm.charAt(lIndex)) != currChar)) 
+		|| 
+		 ((sIndex == 0) && (lIndex > 0) && 
+		(Character.isLetterOrDigit(longForm.charAt(lIndex - 1))))) 
+		     lIndex--; 
+		
+		    if (lIndex < 0) 
+		        return null; 
+		 
+		    lIndex--; 
+		} 
+		
+		lIndex = longForm.lastIndexOf(" ", lIndex) + 1; 
+		return longForm.substring(lIndex); 
+		} 
 	
 /*	public String returnDef(){
 		private int [] qualities = int[indexes.length];
